077180f5528a752f88de681d798c9ba79cef4565
Kyoya2 <emenyrom@protonmail.com>
2026-02-07 23:40:13 +0200

    Classify

--- satisfactory.py
+++ satisfactory.py
@@ -1,18 +1,16 @@
-import functools
-import math
 import re
+import math
 import json
 import string
+import functools
 from enum import Enum
+from typing import Any
 from typing import NamedTuple
 from fractions import Fraction
 from dataclasses import dataclass
 
 from graph import Graph, Node
 
-
-NATIVECLASS_PREFIX = r"/Script/CoreUObject.Class'/Script/FactoryGame."
-ITEM_REGEX = re.compile(r"""\(ItemClass="[\/\w\.']+\.(\w+)'",Amount=(\d+)\)""")
 
 
 class ConveyorBeltType(Enum):
@@ -26,6 +24,12 @@ BEST_UNLOCKED_CONVEYOR_BELT_TYPE = ConveyorBeltType.Mk2
 
 # Corresponds to the "ClassName" element in the JSON file
 GameObjectName = str
+
+# Corresponds to the "NativeClass" element in the JSON
+GameObjectCategoryName = str
+
+# Corresponds to an entry inside the "Classes" member of each top-level entry in the JSON
+GameObject = dict[str, Any]
 
 
 class CountedItem(NamedTuple):
@@ -78,13 +82,13 @@ def unique_string_generator(
             indices.append(0)
 
 
-def to_mermaid(all_classes, graph: Graph):
+def to_mermaid(all_objects, graph: Graph):
     nodes = []
     links = []
 
     def visit_node(node):
-        nonlocal all_classes, nodes
-        item = all_classes[node.data.item_name]
+        nonlocal all_objects, nodes
+        item = all_objects[node.data.item_name]
         nodes.append(f"""{node.data.item_name}["{item['mDisplayName']}<br/>Duration={node.data.production_duration}<br/>Total required={node.data.total_required_amount}<br/>Machined required={node.data.total_machines_required}"]""")
 
     def visit_link(ingredient_node, product_node, amount):
@@ -99,149 +103,163 @@ flowchart-elk TB
 """
 
 
-def generate_recipe_schematic(all_classes, item_name, trivial_resources=tuple()):
-    graph = Graph()
-    nodes: dict[str, Node] = {}
+class SatisfactoryCalculator:
+    _COMMON_NATIVECLASS_PREFIX = r"/Script/CoreUObject.Class'/Script/FactoryGame."
+    _RECIPE_OBJECT_REGEX = re.compile(r"""\(ItemClass="[\/\w\.']+\.(\w+)'",Amount=(\d+)\)""")
 
-    def _generate_recipe_schematic(item_name: GameObjectName):
-        nonlocal trivial_resources, graph, nodes, all_classes
+    def __init__(
+        self,
+        doc_file_path=r"C:\Program Files (x86)\Steam\steamapps\common\Satisfactory\CommunityResources\Docs\en-US.json"
+    ):
+        with open(doc_file_path, 'rb') as f:
+            data = json.load(f)
 
-        if (node := nodes.get(item_name)) is not None:
+        self._all_objects: dict[GameObjectName, GameObject]= {}
+        self._categorized_objects: dict[GameObjectCategoryName, dict[GameObjectName, GameObject]] = {}
+
+        for game_object_category in data:
+            assert game_object_category['NativeClass'].startswith(self._COMMON_NATIVECLASS_PREFIX)
+            native_class = game_object_category['NativeClass'][len(self._COMMON_NATIVECLASS_PREFIX):].rstrip("'")
+
+            current_group_classes = {}
+            for game_object in game_object_category['Classes']:
+                assert game_object['ClassName'] not in self._all_objects
+                self._all_objects[game_object['ClassName']] = game_object
+                current_group_classes[game_object['ClassName']] = game_object
+
+            self._categorized_objects[native_class] = current_group_classes
+
+        self._process_recipes()
+
+    def generate_recipe_schematic(self, item_name: GameObjectName, trivial_resources=tuple()):
+        graph = Graph()
+        nodes: dict[str, Node] = {}
+
+        def _generate_recipe_schematic(item_name: GameObjectName):
+            nonlocal trivial_resources, graph, nodes
+
+            if (node := nodes.get(item_name)) is not None:
+                return node
+
+            obj = self._all_objects[item_name]
+            if 'recipes' not in obj:
+                # No recipes
+                return None
+
+            # Use the first non-alternate recipe
+            recipe = next((recipe for recipe in obj['recipes'] if not recipe.is_alternate), None)
+            if recipe is None:
+                # No non-alternate recipes
+                return None
+
+            node = graph.create_node(ItemDescriptor(item_name, recipe.duration, 0, 0))
+            nodes[item_name] = node
+
+            if item_name not in trivial_resources:
+                for ingredient in recipe.ingredients:
+                    ingredient_node = _generate_recipe_schematic(ingredient.item_name)
+                    node.add_blink(ingredient_node, ingredient.amount)
+
             return node
 
-        obj = all_classes[item_name]
-        if 'recipes' not in obj:
-            # No recipes
-            return None
-        
-        # Use the first non-alternate recipe
-        recipe = next((recipe for recipe in obj['recipes'] if not recipe.is_alternate), None)
-        if recipe is None:
-            # No non-alternate recipes
-            return None
+        root_node = _generate_recipe_schematic(item_name)
+        root_node.data.total_required_amount = 1
 
-        node = graph.create_node(ItemDescriptor(item_name, recipe.duration, 0, 0))
-        nodes[item_name] = node
+        # Fill the "total_required_amount" member of each node
+        # Requires BFS iteration, since this number can be determined for node A
+        # only after it has been determined for ALL its children.
+        cycle_duration = root_node.data.production_duration
 
-        if item_name not in trivial_resources:
-            for ingredient in recipe.ingredients:
-                ingredient_node = _generate_recipe_schematic(ingredient.item_name)
-                node.add_blink(ingredient_node, ingredient.amount)
+        current_nodes = {root_node}
+        visited_nodes = set()
+        machines_required_denominators = set()
+        while True:
+            next_round_nodes = set()
+            for node in current_nodes:
+                if node in visited_nodes:
+                    continue
 
-        return node
+                # If we still haven't visited ALL the flinks, skip this node for now.
+                # We will have more chances to visit it in the required state.
+                if any(flink._target not in visited_nodes for flink in node.flinks()):
+                    continue
 
-    root_node = _generate_recipe_schematic(item_name)
-    root_node.data.total_required_amount = 1
+                visited_nodes.add(node)
 
-    # Fill the "total_required_amount" member of each node
-    # Requires BFS iteration, since this number can be determined for node A
-    # only after it has been determined for ALL its children.
-    cycle_duration = root_node.data.production_duration
+                for flink in node.flinks():
+                    node.data.total_required_amount += flink._target.data.total_required_amount * flink.data
 
-    current_nodes = {root_node}
-    visited_nodes = set()
-    machines_required_denominators = set()
-    while True:
-        next_round_nodes = set()
-        for node in current_nodes:
-            if node in visited_nodes:
+                single_machine_production = cycle_duration / node.data.production_duration
+                node.data.total_machines_required = node.data.total_required_amount / single_machine_production
+                machines_required_denominators.add(
+                    Fraction(node.data.total_machines_required).limit_denominator().denominator)
+
+                next_round_nodes |= {blink._source for blink in node.blinks()}
+
+            next_round_nodes -= visited_nodes
+            if 0 == len(next_round_nodes):
+                break
+
+            current_nodes = next_round_nodes
+
+        # Normalize such that the number of required machines is whole for every item
+        lcm = math.lcm(*machines_required_denominators)
+        if lcm != 1:
+            for node in nodes.values():
+                node.data.total_required_amount *= lcm
+                node.data.total_machines_required *= lcm
+
+        return root_node
+
+    def _process_recipes(self):
+        for recipe in self._categorized_objects['FGRecipe'].values():
+            products = self._RECIPE_OBJECT_REGEX.findall(recipe['mProduct'])
+            if 1 != len(products):
+                assert 2 == len(products)
                 continue
 
-            # If we still haven't visited ALL the flinks, skip this node for now.
-            # We will have more chances to visit it in the required state.
-            if any(flink._target not in visited_nodes for flink in node.flinks()):
-                continue
+            product_name = products[0][0]
+            product_amount = int(products[0][1])
 
-            visited_nodes.add(node)
+            ingredients = self._RECIPE_OBJECT_REGEX.findall(recipe['mIngredients'])
+            ingredients = [CountedItem(item_name, int(amount) / product_amount) for item_name, amount in ingredients]
 
-            for flink in node.flinks():
-                node.data.total_required_amount += flink._target.data.total_required_amount * flink.data
+            duration = float(recipe['mManufactoringDuration'])
+            duration /= product_amount
 
-            single_machine_production = cycle_duration / node.data.production_duration
-            node.data.total_machines_required = node.data.total_required_amount / single_machine_production
-            machines_required_denominators.add(Fraction(node.data.total_machines_required).limit_denominator().denominator)
+            # Factor in the time it takes to load the ingredients onto the constructor/assembler.
+            # Assuming that ingredients are loaded into the machine as a product is being produced, this
+            # makes a difference only if the load time is higher than the production
+            max_amount_ingredient = max(ingredients, default=CountedItem('', 0),
+                                        key=lambda ingredient: ingredient.amount).amount
+            ingredient_load_duration = max_amount_ingredient * (60 / BEST_UNLOCKED_CONVEYOR_BELT_TYPE.value)
+            duration = max(duration, ingredient_load_duration)
 
-            next_round_nodes |= {blink._source for blink in node.blinks()}
+            is_alternate = recipe['mDisplayName'].startswith('Alternate: ')
 
-        next_round_nodes -= visited_nodes
-        if 0 == len(next_round_nodes):
-            break
+            product_obj = self._all_objects[product_name]
+            recipes = product_obj.get('recipes')
+            if recipes is None:
+                recipes = []
+                product_obj['recipes'] = recipes
 
-        current_nodes = next_round_nodes
-
-    # Normalize such that the number of required machines is whole for every item
-    lcm = math.lcm(*machines_required_denominators)
-    if lcm != 1:
-        for node in nodes.values():
-            node.data.total_required_amount *= lcm
-            node.data.total_machines_required *= lcm
-
-    return root_node
-
+            recipes.append(
+                Recipe(
+                    product_name,
+                    ingredients,
+                    duration,
+                    is_alternate,
+                    recipe['ClassName']
+                )
+            )
 
 def main():
-    with open(r"C:\Program Files (x86)\Steam\steamapps\common\Satisfactory\CommunityResources\Docs\en-US.json", 'rb') as f:
-        data = json.load(f)
+    calculator = SatisfactoryCalculator()
 
-    all_classes = {}
-    grouped_classes = {}
+    #root = calculator.generate_recipe_schematic(all_classes, "Desc_SpaceElevatorPart_1_C", trivial_resources=("Desc_IronIngot_C", "Desc_CopperIngot_C"))
+    root = calculator.generate_recipe_schematic("Desc_Rotor_C", trivial_resources=("Desc_IronIngot_C", "Desc_CopperIngot_C"))
 
-    for item in data:
-        assert item['NativeClass'].startswith(NATIVECLASS_PREFIX)
-        native_class = item['NativeClass'][len(NATIVECLASS_PREFIX):].rstrip("'")
-
-        current_group_classes = {}
-        for cls in item['Classes']:
-            assert cls['ClassName'] not in all_classes
-            all_classes[cls['ClassName']] = cls
-            current_group_classes[cls['ClassName']] = cls
-
-        grouped_classes[native_class] = current_group_classes
-    
-    for recipe in grouped_classes['FGRecipe'].values():
-        products = ITEM_REGEX.findall(recipe['mProduct'])
-        if 1 != len(products):
-            assert 2 == len(products)
-            continue
-
-        product_name = products[0][0]
-        product_amount = int(products[0][1])
-
-        ingredients = ITEM_REGEX.findall(recipe['mIngredients'])
-        ingredients = [CountedItem(item_name, int(amount)/product_amount) for item_name, amount in ingredients]
-
-        duration = float(recipe['mManufactoringDuration'])
-        duration /= product_amount
-
-        # Factor in the time it takes to load the ingredients onto the constructor/assembler.
-        # Assuming that ingredients are loaded into the machine as a product is being produced, this
-        # makes a difference only if the load time is higher than the production
-        max_amount_ingredient = max(ingredients, default=CountedItem('', 0), key=lambda ingredient: ingredient.amount).amount
-        ingredient_load_duration = max_amount_ingredient * (60 / BEST_UNLOCKED_CONVEYOR_BELT_TYPE.value)
-        duration = max(duration, ingredient_load_duration)
-
-        is_alternate = recipe['mDisplayName'].startswith('Alternate: ')
-
-        product_obj = all_classes[product_name]
-        recipes = product_obj.get('recipes')
-        if recipes is None:
-            recipes = []
-            product_obj['recipes'] = recipes
-
-        recipes.append(
-            Recipe(
-                product_name,
-                ingredients,
-                duration,
-                is_alternate,
-                recipe['ClassName']
-            )
-        )
-
-    #root = generate_recipe_schematic(all_classes, "Desc_SpaceElevatorPart_1_C", trivial_resources=("Desc_IronIngot_C", "Desc_CopperIngot_C"))
-    root = generate_recipe_schematic(all_classes, "Desc_Rotor_C", trivial_resources=("Desc_IronIngot_C", "Desc_CopperIngot_C"))
-
-    print(to_mermaid(all_classes, root._graph))
+    print(to_mermaid(calculator._all_objects, root._graph))
 
 
 if __name__ == '__main__':
